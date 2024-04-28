02e94dbb507be6f5138c6814b19dfe77de6e1cb2
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-04-28 18:33:56 -0300

    fiz `/migrations` endpoint connection bug`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,20 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database.js";
+import { error } from "node:console";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method} not allowed"`,
+    });
+  }
+  let dbClient;
+
+  try {
+   dbClient = await database.getNewClient();
 
   const defaultMigrationsOptions = {
     dbClient: dbClient,
@@ -16,7 +27,6 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -26,13 +36,15 @@ export default async function migrations(request, response) {
       dryRun: false,
     });
 
-    await dbClient.end();
-
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
     return response.status(200).json(migratedMigrations);
   }
-
-  return response.status(405).end();
+  }catch (err) {
+    console.error(error)
+    throw error;
+  } finally{
+    await dbClient.end();
+  }
 }
