a0a28514ac3419fc3ba9198cfcb5277e43a59d50
João Pedro Alexandrino <131566983+jphalexandrino@users.noreply.github.com>
2025-05-29 15:48:32 -0300

    test: rename all descriptions to use new pattern

--- test/integrations/api/v1/migrations/post.test.js
+++ test/integrations/api/v1/migrations/post.test.js
@@ -6,29 +6,41 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const liveRunMigrations = async () =>
-    await fetch("http://localhost:3000/api/v1/migrations", {
-      method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymus user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const liveRunMigrations = async () =>
+          await fetch("http://localhost:3000/api/v1/migrations", {
+            method: "POST",
+          });
+
+        // First Response = Verificação de se as migrations foram enviadas.
+        const firstResponse = await liveRunMigrations();
+        expect(firstResponse.status).toBe(201);
+        const firstResponseBody = await firstResponse.json();
+        expect(Array.isArray(firstResponseBody)).toBe(true);
+        expect(firstResponseBody.length).toBeGreaterThan(0);
+
+        //Confere o corpo da primeira resposta.
+        //console.log("First response body:", firstResponseBody);
+      });
+      test("For the second time", async () => {
+        const liveRunMigrations = async () =>
+          await fetch("http://localhost:3000/api/v1/migrations", {
+            method: "POST",
+          });
+
+        // Second Response = Verifica se retornou alguma migration (não deve...).
+        const secondResponse = await liveRunMigrations();
+        expect(secondResponse.status).toBe(200);
+        const secondResponseBody = await secondResponse.json();
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+
+        // Confere o Body da segunda resposta.
+        //console.log("Second response body:", secondResponseBody);
+      });
     });
-
-  // First Response = Verificação de se as migrations foram enviadas.
-  const firstResponse = await liveRunMigrations();
-  expect(firstResponse.status).toBe(201);
-  const firstResponseBody = await firstResponse.json();
-  expect(Array.isArray(firstResponseBody)).toBe(true);
-  expect(firstResponseBody.length).toBeGreaterThan(0);
-
-  //Confere o corpo da primeira resposta.
-  //console.log("First response body:", firstResponseBody);
-
-  // Second Response = Verifica se retornou alguma migration (não deve...).
-  const secondResponse = await liveRunMigrations();
-  expect(secondResponse.status).toBe(200);
-  const secondResponseBody = await secondResponse.json();
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
-
-  // Confere o Body da segunda resposta.
-  //console.log("Second response body:", secondResponseBody);
+  });
 });
